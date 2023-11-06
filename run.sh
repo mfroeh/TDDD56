170914749bd5ae84dc1e3243d03ab5621857cb2f
Samuel Yute <samiyutez@gmail.com>
2023-11-06 13:13:36 +0200

    Initial commit

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+output_data=()
+
+# Loop for 8 iterations
+for index in {0..16}
+do
+    echo "Iteration $index"
+
+    # Run the commands and save the output to a file
+    make clean
+    rm mandelbrot.ppm
+    make MEASURE=1 NB_THREADS=$index LOADBALANCE=1
+    output_data+=("$(./mandelbrot-*)")
+
+    echo "Iteration $index completed"
+done
+
+printf '%s\n' "${output_data[@]}" > output.txt
