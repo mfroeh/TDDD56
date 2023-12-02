6e3c056597f37ef4177483752a6683db1edbcf83
mfroeh <mfroeh0@pm.me>
2023-12-02 09:51:51 +0100

    Adds shared memory for singly threaded blocks

--- lab5/filter.cu
+++ lab5/filter.cu
@@ -36,6 +36,8 @@ struct pixel {
 // Use these for setting shared memory size.
 #define maxKernelSizeX 10
 #define maxKernelSizeY 10
+#define BLOCK_HEIGHT (maxKernelSizeX * 2 + 1)
+#define BLOCK_WIDTH (maxKernelSizeY * 2 + 1)
 
 __global__ void filter(pixel *image, pixel *out, const unsigned int imagesizex,
                        const unsigned int imagesizey, const int kernelsizex,
@@ -43,6 +45,16 @@ __global__ void filter(pixel *image, pixel *out, const unsigned int imagesizex,
   // map from blockIdx to pixel position
   int x = blockIdx.x * blockDim.x + threadIdx.x;
   int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+  __shared__ pixel block[BLOCK_HEIGHT][BLOCK_WIDTH];
+
+  for (int dy{-kernelsizey}, i{}; dy <= kernelsizey; ++dy, ++i) {
+    for (int dx{-kernelsizex}, j{}; dx <= kernelsizex; ++dx, ++j) {
+      int yy{min(max(y + dy, 0), static_cast<int>(imagesizey) - 1)};
+      int xx{min(max(x + dx, 0), static_cast<int>(imagesizex) - 1)};
+      block[i][j] = image[yy * imagesizex + xx];
+    }
+  }
 
   int dy, dx;
   unsigned int sumx, sumy, sumz;
@@ -69,6 +81,8 @@ __global__ void filter(pixel *image, pixel *out, const unsigned int imagesizex,
     out[(y * imagesizex + x)].r = sumx / divby;
     out[(y * imagesizex + x)].g = sumy / divby;
     out[(y * imagesizex + x)].b = sumz / divby;
+  } else {
+    printf("%d,%d is outside imagesize!", x, y);
   }
 }
 
@@ -91,9 +105,12 @@ void computeImages(int kernelsizex, int kernelsizey) {
   cudaMemcpy(dev_input, image, imagesizey * imagesizex * 3,
              cudaMemcpyHostToDevice);
   cudaMalloc((void **)&dev_bitmap, imagesizex * imagesizey * 3);
-  dim3 grid(imagesizex, imagesizey);
-  filter<<<grid, 1>>>(dev_input, dev_bitmap, imagesizex, imagesizey,
-                      kernelsizex, kernelsizey);  // Awful load balance
+
+  dim3 grid_dim{imagesizex, imagesizey};
+  dim3 block_dim{1, 1};
+  filter<<<grid_dim, block_dim>>>(dev_input, dev_bitmap, imagesizex, imagesizey,
+                                  kernelsizex,
+                                  kernelsizey);  // Awful load balance
   cudaDeviceSynchronize();
   //	Check for errors!
   cudaError_t err = cudaGetLastError();
@@ -110,8 +127,8 @@ void Draw() {
   glClearColor(0.0, 0.0, 0.0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT);
 
-  if (imagesizey >=
-      imagesizex) {  // Not wide - probably square. Original left, result right.
+  if (imagesizey >= imagesizex) {  // Not wide - probably square. Original
+                                   // left, result right.
     glRasterPos2f(-1, -1);
     glDrawPixels(imagesizex, imagesizey, GL_RGB, GL_UNSIGNED_BYTE, image);
     glRasterPos2i(0, -1);
